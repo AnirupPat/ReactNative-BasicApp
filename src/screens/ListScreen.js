71b0b12bb40a0932c6f536d1b702fc399112a7a5
Anirup Patnaik <anirup049@gmail.com>
2020-05-03 02:45:05 +0530

    Rendering a List

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
 const ListScreen = () => {
+    const friends = [
+        {
+            name: 'Sangram'
+        },{
+            name: 'Gangotree'
+        },{
+            name: 'Rutu'
+        },{
+            name: 'Sharan'
+        },{
+            name: 'Adarsh'
+        },{
+            name: 'Rajan'
+        },{
+            name: 'Aish'
+        },{
+            name: 'Tapas'
+        },{
+            name: 'Pujary'
+        }
+    ]
     return (
-        <Text>List Screen</Text>
+        // we have used object destructuring here
+        <FlatList 
+            data={friends} 
+            renderItem={({ item }) => {
+            return <Text>{item.name}</Text>
+        }}/>
     );
 };
 
