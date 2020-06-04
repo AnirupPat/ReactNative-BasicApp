74913085757762955f0ab3959edefa273d32dbb2
Anirup Patnaik <anirup049@gmail.com>
2020-06-04 11:51:33 +0530

    JUstify content and align items in flex

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,7 +4,9 @@ import { View, StyleSheet, Text } from 'react-native';
 const BoxScreen = () => {
     return (
         <View style={style.viewStyle}>
-            <Text style={style.textStyle}>Box Screen</Text>
+            <Text style={style.textStyle}>Child #1</Text>
+            <Text style={style.textStyle}>Child #2</Text>
+            <Text style={style.textStyle}>Child #3</Text>
         </View>
     )
 };
@@ -12,12 +14,16 @@ const BoxScreen = () => {
 const style = StyleSheet.create({
     viewStyle: {
         borderWidth: 1,
-        borderColor: 'black'
+        borderColor: 'black',
+       // alignItems: "center",
+       justifyContent: "space-around",
+        flexDirection: "row",
+        height: 200
     },
     textStyle: {
-        borderWidth: 10,
+        borderWidth: 5,
         borderColor: 'red',
-        margin: 20
+        height: 50
     }
 });
 
